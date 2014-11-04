afd71459c8a5462a9978dc0b4974a14d2629ef4d
YOUR-NAME <YOUR-EMAIL@DOMAIN.COM>
2014-11-04 01:35:39 -0500

    Added templating

--- flask_trial/hello.py
+++ flask_trial/hello.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def hello_world():
     return "Hello World"
 
+@app.route("/<username>")
+def hello(username):
+    return "Hello, %s!" % username
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_name(name=None):
+    return render_template("hello.html", name = name)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
